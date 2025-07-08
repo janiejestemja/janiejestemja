c0c114523dd07c3307b47d7d247675cfdc9bc4fb
janiejestemja <janiejestemja@email>
2025-07-08 19:10:30 +0200

    extended nvim config

--- init.lua
+++ init.lua
@@ -2,9 +2,10 @@ vim.opt.rtp:prepend("~/.config/nvim/lazy/lazy.nvim")
 
 require("lazy").setup({
     { "nvim-lualine/lualine.nvim", dependencies = "nvim-tree/nvim-web-devicons" },
-    { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate" },
+    { "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" },
     { "catppuccin/nvim", name = "catppuccin" },
     { "nvim-tree/nvim-tree.lua", lazy = false, dependencies = "nvim-tree/nvim-web-devicons" },
+	{ "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
 })
 
 require("nvim-tree").setup({})
@@ -19,22 +20,31 @@ require("lualine").setup({
 require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true,
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = true,
   },
 })
 
 vim.cmd.colorscheme("catppuccin-mocha")
 
-vim.api.nvim_set_keymap("n", "<leader>tt", ":belowright split | resize 8 | terminal<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>]], {noremap = true})
+-- Mapping keys
+vim.g.mapleader = "?"
 
+-- NvimTree
 vim.api.nvim_set_keymap("n", "<leader>to", ":NvimTreeOpen<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>tc", ":NvimTreeClose<CR>", {noremap = true, silent = true})
 
+-- Terminal
+vim.api.nvim_set_keymap("n", "<leader>tt", ":belowright split | resize 8 | terminal<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>]], {noremap = true})
+
+-- Telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+-- Classic vim
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
